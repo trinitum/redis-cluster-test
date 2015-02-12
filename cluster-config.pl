f49a34a2dab120e4623f558cf4b2f59d0ea0fe32
Pavel Shaydo <zwon@trinitum.org>
2015-02-13 00:21:58 +0100

    use RedisDB::Cluster functionality to add nodes

--- cluster-config.pl
+++ cluster-config.pl
@@ -3,37 +3,50 @@ use 5.010;
 use strict;
 use warnings;
 
-use RedisDB;
+use RedisDB::Cluster;
 use List::Util qw(min);
 
 my @node;
 my $slot           = 0;
 my $MAXSLOT        = 16383;
 my $slots_per_node = int( ( $MAXSLOT + 1 ) / 3 + 1 );
+my $cluster        = RedisDB::Cluster->new(
+    startup_nodes => [
+        {
+            host => '127.0.0.1',
+            port => 7000
+        }
+    ],
+);
+
 my $cluster_nodes;
 for ( 0 .. 5 ) {
-    say "Initializing '127.0.0.1:700$_'";
-    $node[$_] = RedisDB->new(
+    my $new_node = {
         host => '127.0.0.1',
         port => 7000 + $_
-    );
-    if ($_) {
-        print "Introducing new node: ";
-        say $node[$_]->cluster( 'MEET', '127.0.0.1', 7000 + $_ - 1 );
-        $cluster_nodes = $node[$_]->cluster_nodes();
-    }
+    };
+    say "Initializing '127.0.0.1:700$_'";
     if ( $_ % 2 ) {
-        my $master_addr = "127.0.0.1:700" . ($_-1);
-        my ($master) =
+        my $master_addr = "127.0.0.1:700" . ( $_ - 1 );
+        my ($master_id) =
           map  { $_->{node_id} }
           grep { $_->{address} eq $master_addr } @$cluster_nodes;
-        print "Replicating from $master: ";
-        say $node[$_]->cluster( 'REPLICATE', $master );
+        print "Adding new node as a replica of $master_id ($master_addr): ";
+        say $cluster->add_new_node( $new_node, $master_id );
     }
     else {
+        if ($_) {
+            print "Adding new node as a master: ";
+            say $cluster->add_new_node($new_node);
+        }
         my @slots = $slot .. min( $slot + $slots_per_node, $MAXSLOT );
-        $slot += $slots_per_node;
+        $slot += $slots_per_node + 1;
+        my $redis = RedisDB->new($new_node);
         print "Adding slots: ";
-        say $node[$_]->cluster( 'ADDSLOTS', @slots );
+        say $redis->cluster( 'ADDSLOTS', @slots );
+        $cluster_nodes = $redis->cluster_nodes;
     }
 }
+
+sleep 3;
+say $cluster->random_connection->cluster('nodes');
