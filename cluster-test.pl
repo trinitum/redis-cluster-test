da85cf46e65ed037ccd4163fb1799fb875c9142b
Pavel Shaydo <zwon@trinitum.org>
2015-02-15 00:28:28 +0100

    add keys that go to three slots to cluster-test

--- cluster-test.pl
+++ cluster-test.pl
@@ -15,11 +15,21 @@ my $redis = RedisDB::Cluster->new(
     ]
 );
 
+$RedisDB::Cluster::DEBUG=1;
+
+my $n = 0;
 while ( 1 ) {
     my $i = int rand 16383;
     my $res = $redis->execute( "set", "foo$i", $i );
     warn $res unless "$res" eq 'OK';
     my $got = $redis->execute( "get", "foo$i" );
     warn "got: $got instead of $i" unless "$got" eq $i;
+
+    ($n %= 3)++;
+    $res = $redis->execute( "set", "{hash$n}$i", $i);
+    warn $res unless "$res" eq 'OK';
+    $res = $redis->execute("get", "{hash$n}$i");
+    warn "got: $res instead of expected $i" unless "$got" eq $i;
+
     usleep 100_000;
 }
