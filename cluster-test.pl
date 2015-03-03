957f88d74efeb773cd31bd736941f2e8c6f3250d
Pavel Shaydo <zwon@trinitum.org>
2015-03-03 23:27:43 +0100

    replace execute with wrappers

--- cluster-test.pl
+++ cluster-test.pl
@@ -12,23 +12,23 @@ my $redis = RedisDB::Cluster->new(
             host => '127.0.0.1',
             port => '7000'
         }
-    ]
+    ],
 );
 
-$RedisDB::Cluster::DEBUG=1;
+$RedisDB::Cluster::DEBUG = 1;
 
 my $n = 0;
-while ( 1 ) {
+while (1) {
     my $i = int rand 16383;
     my $res = $redis->execute( "set", "foo$i", $i );
     warn $res unless "$res" eq 'OK';
     my $got = $redis->execute( "get", "foo$i" );
     warn "got: $got instead of $i" unless "$got" eq $i;
 
-    ($n %= 3)++;
-    $res = $redis->execute( "set", "{hash$n}$i", $i);
+    ( $n %= 3 )++;
+    $res = $redis->set( "{hash$n}$i", $i );
     warn $res unless "$res" eq 'OK';
-    $res = $redis->execute("get", "{hash$n}$i");
+    $res = $redis->get("{hash$n}$i");
     warn "got: $res instead of expected $i" unless "$got" eq $i;
 
     usleep 100_000;
